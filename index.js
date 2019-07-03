6301bce3a5e10584bce6d8290831db458a84c31e
Ivan Brusniak <ivan.brusniak@gmail.com>
2019-07-03 19:54:47 +0300

    Use http module

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 
+const http = require("http");
 
-console.log("Initial :)");
+http.createServer(function(request, response) {
+
+    response.end("Server response.");
+}).listen(3000, "127.0.0.1", function(){
+
+    console.log("Server listening on 3000");
+});
