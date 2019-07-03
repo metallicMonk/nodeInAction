76a60148202218f46d2ee249d7271bd2150be681
Ivan Brusniak <ivan.brusniak@gmail.com>
2019-07-03 19:30:51 +0300

    Restart repository. Delete all old files!

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const net = require('net');
-const server = net.createServer(socket => {
-    socket.on('data', data => {
-        socket.write(data);
-    });
-});
-server.listen(8888);
+
+
+console.log("Initial :)");
