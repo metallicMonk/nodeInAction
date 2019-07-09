c508a1d49b16a2dea7c94b7be3952ba399ce7cd7
Ivan Brusniak <ivan.brusniak@gmail.com>
2019-07-09 18:41:26 +0300

    Use simple routing with "req.url" propery

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const http = require('http');
 
 function handler(req, res) {
 
-	res.setHeader("UserId", 333);
+	
 	res.setHeader("Content-Type", "text/html; charset=utf-8;");
-	res.write("<h2>Hi there!</h2>");
+	
+	const url = req.url;
+
+	if (url === "/home" || url === "/") {
+		res.write("<h1>Home</h1>");
+	} else if (url == "/about") {
+		res.write("<h1>About</h1>");
+	} else {
+		res.write("<h1>Unknown!</h1>");
+	}
 
 	res.end();
 }
