797aa8014061f564e869998cafe21a69cd42277a
Ivan Brusniak <ivan.brusniak@gmail.com>
2019-07-24 19:59:44 +0300

    Add routers

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ const express = require('express');
 
 const app = express();
 
+const responses = {
+    "/": "<h1>Main page</h1>",
+    "/about": "<h1>About page</h1>",
+    "/contact": "<h1>Contact page</h1>"
+};
+
 app.get('/', function (request, response) {
-    if(Object.keys(request.query).indexOf('exit') != -1) throw new Error('Exiting!!!');
-    response.send("<h2>Hello express framework!</h2>");
+    response.send(responses['/']);
+});
+
+app.get('/about', function (request, response) {
+    response.send(responses['/about']);
+});
+
+app.get('/contact', function (request, response) {
+    response.send(responses['/contact']);
 });
 
 app.listen(3000, function () {
