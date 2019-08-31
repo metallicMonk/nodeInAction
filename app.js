e9d55dcac62c7b900a694883fd977f1f69d37b9b
Ivan Brusniak <ivan.brusniak@gmail.com>
2019-08-31 22:42:53 +0300

    Change app.js

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 
 const app = require('express')();
-const fs = require('fs');
-
-app.use(function (request, response, next) {
-    let now = new Date();
-    let hour = now.getHours();
-    let minute = now.getMinutes();
-    let seconds = now.getSeconds();
-    let data = `${hour}:${minute}:${seconds} ${request.method} ${request.url} ${request.get('user-agent')}`;
-
-    console.log(data);
-    fs.appendFile('server.log', data + '\n', function () { });
-    next();
-});
 
 app.get('/', function (request, response) {
-    response.send('Get route /');
+    response.send(JSON.stringify({
+        status: "OK",
+    }, null, 4));
 });
 
 app.listen(8080, function () {
